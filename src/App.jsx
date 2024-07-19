f6102856a16f99da00eae6960174a0cd8e9bb57d
muhammed ajmal <muhammedajmalcc6424094@gmail.com>
2024-07-19 15:42:24 +0530

    create chat app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {ChatEngine} from 'react-chat-engine'
+import "./App.css"
+const App = () => {
+  return (
+    <div>
+      <ChatEngine
+      height= '100vh'
+      projectID = "785d18ea-bd54-4f0f-82ec-2397dd613984"
+      userName ="juu"
+      userSecrete ="muhammed@cc"
+      
+      />
+    </div>
+  );
+}
+
+export default App;
